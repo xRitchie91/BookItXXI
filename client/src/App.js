bcb0a66bc6f07dad84d3a57d3e222de3d53ad0b6
Ritchie <74946954+xRitchie91@users.noreply.github.com>
2021-07-30 03:33:51 -0500

    Updated src/app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,43 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { ApolloProvider, ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client'
+import { setContext } from '@apollo/client/link/context'
+
 import SearchBooks from './pages/SearchBooks';
 import SavedBooks from './pages/SavedBooks';
 import Navbar from './components/Navbar';
 
+
+// establish new link to the server at /graphql endpoint
+const httpLink = createHttpLink({
+  // location for backend server
+  // react server enviroment runs at localhost:3000
+  uri: '/graphql'
+})
+
+// create a 'middleware' function that retrieves token and combines it with existing httpLink
+const authLink = setContext((_, {headers}) => {
+const token = localStorage.getItem('id_token')
+return {
+  headers : {
+    ...headers,
+    authorization: token ? `Bearer ${token}` : '',
+  },
+}
+})
+
+// instantiate apollo and create connection to endpoint
+// also create new cache object
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache()
+})
+
 function App() {
+
   return (
-    <Router>
+    <ApolloProvider client={client}>
+      <Router>
       <>
         <Navbar />
         <Switch>
@@ -16,6 +47,7 @@ function App() {
         </Switch>
       </>
     </Router>
+    </ApolloProvider>
   );
 }
 
