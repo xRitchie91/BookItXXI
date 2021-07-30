41d764a028b0531f19dc998c1a7d09e98986110a
Ritchie <74946954+xRitchie91@users.noreply.github.com>
2021-07-30 03:57:18 -0500

    Updated typedefs.js

--- server/schema/typedefs.js
+++ server/schema/typedefs.js
@@ -23,3 +23,14 @@ const typeDefs = gql`
         token: ID!
         user: User
     }
+    type Query {
+        users: [User]
+        me: User
+    }
+    type Mutation {
+        login(email: String!, password: String!): Auth
+        addUser(username: String!, email: String!, password: String!): Auth
+        saveBook(bookId: ID, authors: [String], description: String, title: String, image: String, link: String): User
+        removeBook(bookId: String!): User
+    }
+`;
