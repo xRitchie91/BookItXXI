dac84fb6374438bae7d92bf8844b413712c3a76d
Ritchie <74946954+xRitchie91@users.noreply.github.com>
2021-07-30 03:18:24 -0500

    added queries.js file

---
+++ client/src/utils/queries.js
@@ -0,0 +1,20 @@
+mport { gql } from '@apollo/client';
+
+export const GET_ME = gql`
+    {
+        me {
+            _id
+            username
+            email
+            bookCount
+            savedBooks {
+                bookId
+                authors
+                image
+                description
+                title
+                link
+            }
+        }
+    }
+`;
