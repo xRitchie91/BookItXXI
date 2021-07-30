dc02766670527749018f1d0a819f86c4c3e4d89d
Ritchie <74946954+xRitchie91@users.noreply.github.com>
2021-07-30 03:47:54 -0500

    Updated resolvers.js

--- server/schema/resolvers.js
+++ server/schema/resolvers.js
@@ -32,6 +32,35 @@ const resolvers = {
             const token = signToken(user)
             return { user, token }
         },
-        
-        
+        addUser: async(parent, args) => {
+            const user = await User.create(args)
+            const token = signToken(user)
+            return { user, token }
+        },
+        saveBook: async (parent, { bookId, authors, description, title, image, link }, context ) => {
+            if(context.user){
+                const updateUser = await User.findOneAndUpdate(
+                    { _id: context.user._id },
+                    { $push: { savedBooks: { bookId, authors, description, title, image, link } } },
+                    { new: true }
+                    )
+                return updateUser
+            }
+            throw new  AuthenticationError('Cannot do that til you log in!')
+        },
+        removeBook: async (parent, { bookId }, context) => {
+            if(context.user){
+                const updateUser = await User.findOneAndUpdate(
+                    { _id: context.user._id },
+                    { $pull: { savedBooks: { bookId: bookId } } },
+                    { new: true }
+                    )
+                return updateUser
+            }
+            throw new AuthenticationError('Cannot do that til you log in!')
+        }
+    }
+}
+
+
 module.exports = resolvers;
