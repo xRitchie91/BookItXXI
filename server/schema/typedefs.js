64630ca420ff5d910a2e3367c8446c936c8cdca8
Ritchie <74946954+xRitchie91@users.noreply.github.com>
2021-07-30 03:55:10 -0500

    Updated typedefs

--- server/schema/typedefs.js
+++ server/schema/typedefs.js
@@ -10,3 +10,16 @@ const typeDefs = gql`
         savedBooks: [Book]
         bookCount: Int
     }
+    type Book {
+        _id: ID
+        authors: [String]
+        description: String
+        bookId: String
+        image: String
+        link: String
+        title: String
+    }
+    type Auth {
+        token: ID!
+        user: User
+    }
